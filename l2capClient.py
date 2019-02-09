a2af60167934d6f8badf77ab91142398d647b772
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-02-09 14:22:59 -0600

    Updated client. Added conditionals to exit and print error statements if the camera and / or server are not availale

--- l2capClient.py
+++ l2capClient.py
@@ -1,4 +1,3 @@
-
 import bluetooth
 
 from picamera.array import PiRGBArray
@@ -7,32 +6,46 @@ import time
 import cv2
 
 def main():
-	resolution = [640, 480] # resolution of camera
-	frameRate = 32  # frame rate of camera
+        resolution = [640, 480] # resolution of camera
+        frameRate = 32
 
-  	bd_addr = "B8:27:EB:DC:E7:05" # server mac address
-  	port = 0x1001 # default bluetooth port, on client
+        bd_addr = "B8:27:EB:DC:E7:05"
+        port = 0x1001 # default bluetooth port, on client
 
-  	camera, rawCapture = initCamera(resolution, frameRate)  # set up the camera
-  	sock = connectToServer(bd_addr, port) # connect to the server
+        try:
+                camera, rawCapture = initCamera(resolution, frameRate)
+        except:
+            print ("Error Initializing Camera")
 
+        sock = connectToServer(bd_addr, port) # connect to the server
+        if (sock == 0):
+            return
   	# sendImageStream(sock)
 
-  	sock.send("hello!!")  # send sample message
-  	sock.close()  # close socket
+      
+        sock.send("hello!!")  # send sample message
+        sock.close()  # close socket
   
 def initCamera(resolution, frameRate):
-  	camera = PiCamera()
-  	camera.resolution = (resolution[0], resolution[1])
-  	camera.framerate = frameRate
-  	rawCapture = PiRGBArray(camera, size=(resolution[0], resolution[1]))
-  	time.sleep(0.1)
-  	return camera, rawCapture
+        try:
+                camera = PiCamera()
+        except:
+                print("THERE WAS AN ERROR")
+                return
+        camera.resolution = (resolution[0], resolution[1])
+        camera.framerate = frameRate
+        rawCapture = PiRGBArray(camera, size=(resolution[0], resolution[1]))
+        time.sleep(0.1)
+        return camera, rawCapture
 
 def connectToServer(address, port):
-  	sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-  	sock.connect((bd_addr, port))
-  	return sock
+        sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+        try:
+            sock.connect((address, port))
+        except:
+            print("Unable to connect to server")
+            sock = 0
+        return sock
 
 def sendImageStream(socket):
 	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
