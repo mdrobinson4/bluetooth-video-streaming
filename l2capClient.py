e61b72b177ba41339343d1cedaa3544614793ddf
Mark Robinson <mdrobinson4@crimson.ua.edu>
2019-02-12 01:09:02 -0600

    Made corrections to server and client code

--- l2capClient.py
+++ l2capClient.py
@@ -6,60 +6,52 @@ import time
 import cv2
 
 def main():
-        resolution = [640, 480] # resolution of camera
-        frameRate = 32
+    resolution = [640, 480] # resolution of camera
+    frameRate = 32
 
-        bd_addr = "B8:27:EB:DC:E7:05"
-        port = 0x1001 # default bluetooth port, on client
+    bd_addr = "B8:27:EB:DC:E7:05"
+    port = 0x1001 # default bluetooth port, on client
 
-        try:
-                camera, rawCapture = initCamera(resolution, frameRate)
-        except:
-            print ("Error Initializing Camera")
+    try:
+        socket = connectToServer(bd_addr, port) # connect to the server
+    except:
+        print ("Unable To Connect To Server")
+        return
 
-        sock = connectToServer(bd_addr, port) # connect to the server
-        if (sock == 0):
-            return
-  	# sendImageStream(sock)
+    try:
+        camera, rawCapture = initCamera(resolution, frameRate)
+        sendImageStream(socket, camera) # Send image stream to server
+    except:
+        print ("Error Initializing Camera, Sending Message Instead")
+        socket.send("Hello World!!") # Send message to server
 
-      
-        sock.send("hello!!")  # send sample message
-        sock.close()  # close socket
+    sock.close()  # close socket
   
 def initCamera(resolution, frameRate):
-        try:
-                camera = PiCamera()
-        except:
-                print("THERE WAS AN ERROR")
-                return
-        camera.resolution = (resolution[0], resolution[1])
-        camera.framerate = frameRate
-        rawCapture = PiRGBArray(camera, size=(resolution[0], resolution[1]))
-        time.sleep(0.1)
-        return camera, rawCapture
+    camera = PiCamera()
+    camera.resolution = (resolution[0], resolution[1])
+    camera.framerate = frameRate
+    rawCapture = PiRGBArray(camera, size=(resolution[0], resolution[1]))
+    time.sleep(0.1)
+    return camera, rawCapture
 
 def connectToServer(address, port):
-        sock = bluetooth.BluetoothSocket(bluetooth.L2CAP)
-        try:
-            sock.connect((address, port))
-        except:
-            print("Unable to connect to server")
-            sock = 0
-        return sock
+    socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
+    socket.connect((address, port))
+    return socket
 
-def sendImageStream(socket):
-	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-		# grab the raw NumPy array representing the image, then initialize the timestamp
-		# and occupied/unoccupied text
-		image = frame.array
-
-		# show the frame
-		# cv2.imshow("Video Stream", image)
-		key = cv2.waitKey(1) & 0xFF
-
-		# clear the stream in preparation for the next frame
-		rawCapture.truncate(0)
-		socket.send(image)
+def sendImageStream(socket, camera, rawCapture):
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image,
+        # then initialize the timestamp
+	# and occupied/unoccupied text
+        image = frame.array
+	# show the frame
+    	# cv2.imshow("Video Stream", image)
+        key = cv2.waitKey(1) & 0xFF
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+        socket.send(image)
   
-if __name__== "__main__":
+if __name__ == "__main__":
   	main()
--- l2capServer.py
+++ l2capServer.py
@@ -5,21 +5,26 @@ from picamera import PiCamera
 import time
 import cv2
 
-port = 0x1001 # select first bluetooth adapter on server
+def main():
+    port = 0x1001 # select first bluetooth adapter on server
 
-serverSocket = openSocket(port) # open server socket
-serverSocket.listen(1)  # listen for the client's connection request
+    serverSocket = openSocket(port) # open server socket
+    serverSocket.listen(1)  # listen for the client's connection request
 
-clientSocket, address = serverSocket.accept() # allow the client to connect
-print("Accepted Connection From: ", address)
+    clientSocket, address = serverSocket.accept() # allow the client to connect
+    print("Accepted Connection From: ", address)
 
-data = clientSocket.recv(1024)  # get the data from the client
-print "received [%s]" % data
+    data = clientSocket.recv(1024)  # get the data from the client
+    print ("received [%s]" % data)
 
-clientSocket.close()  # close the client socket
-serverSocket.close()  # close the server socket
+    clientSocket.close()  # close the client socket
+    serverSocket.close()  # close the server socket
 
 def openSocket(port):
-    serverSocket = bluetooth.BluetoothSocket(bluetooth.L2CAP) # create bluetooth object and set l2cap (udp) as the transportation service
+    serverSocket = bluetooth.BluetoothSocket(bluetooth.L2CAP) 
+    # create bluetooth object and set 
+    # l2cap (udp) as the transportation service
     serverSocket.bind(("", port)) # bind to default bluetooth adapter on server
     return serverSocket
+
+main()
